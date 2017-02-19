ebda330dc16aa8a7cd4e746248c7995580cfc2a9
mengalong <alongmeng@gmail.com>
2017-02-20 00:42:20 +0800

    add the process notify event

--- multiple_process_notify_event/tar.py
+++ multiple_process_notify_event/tar.py
@@ -1,12 +1,28 @@
 #encoding:utf8
+import tarfile
+import os
 from xml.etree.ElementTree import ElementTree, Element
 import csv
 from StringIO import  StringIO
 import requests
 from threading import Thread
+from threading import Event
+
 
 # Queue 是一个线程安全的队列数据结构
 from Queue import Queue
+
+'''
+实现一个线程，将转换出的xml文件压缩打包，比如转换线程每生产处100个xml文件
+就同通知打包线程进行打包，并删除xml文件。打包完成后，打包线程同坐转换线程
+转换线程继续转换
+
+线程间的时间通知可以使用 Threading.Event
+1. 等待时间一段调用wait，等待事件
+2. 通知一端调用set，通知事件
+'''
+
+
 
 def pretty(e, level=0):
     if len(e) > 0:
@@ -15,7 +31,6 @@ def pretty(e, level=0):
             pretty(child, level + 1)
         child.tail = child.tail[:-1]
     e.tail = '\n' + '\t' * level
-
 
 
 class DownloadThread(Thread):
@@ -41,9 +56,11 @@ class DownloadThread(Thread):
         self.queue.put((self.sid, data))
 
 class ConvertThread(Thread):
-    def __init__(self, queue):
+    def __init__(self, queue, cEvent, tEvent):
         Thread.__init__(self)
         self.queue = queue
+        self.cEvent = cEvent
+        self.tEvent = tEvent
 
     def csvToXml(self, scsv, fxml):
         reader = csv.reader(scsv)
@@ -65,28 +82,76 @@ class ConvertThread(Thread):
 
     def run(self):
 
+        count = 0
         while True:
             # 接收线程传递过来的数据 sid, data
             sid, data = self.queue.get()
             print "convert %s" % sid
             if sid == -1:
+                self.cEvent.set()
+                self.tEvent.wait()
                 break
             if data:
                 fname = str(sid).rjust(6, '0') + ".xml"
                 with open(fname, 'wb') as wf:
                     self.csvToXml(data, wf)
+                count += 1
+                if count == 5:
+                    self.cEvent.set()
+                    self.tEvent.wait()
+                    self.tEvent.clear()
+                    count = 0
 
-q = Queue()
-dThreads = [DownloadThread(i, q) for i in xrange(1, 3)]
-cThread = ConvertThread(q)
+class TarThread(Thread):
+    def __init__(self, cEvent, tEvent):
+        Thread.__init__(self)
+        self.count = 0
+        self.cEvent = cEvent
+        self.tEvent = tEvent
+        self.setDaemon(True)
 
-for t in dThreads:
-    t.start()
+    def tarXML(self):
+        self.count += 1
+        tfname = '%d.tgz' % self.count
+        tf = tarfile.open(tfname, 'w:gz')
+        for fname in os.listdir('.'):
+            if fname.endswith(".xml"):
+                tf.add(fname)
+                os.remove(fname)
+        tf.close()
 
-cThread.start()
+        if not tf.members:
+            os.remove(tf)
 
-for t in dThreads:
-    t.join()
+        print "create tar %s" % self.count
+
+    def run(self):
+        while True:
+            self.cEvent.wait()
+            self.tarXML()
+            self.cEvent.clear()
+            self.tEvent.set()
+
+
+
+if __name__ == "__main__":
+    q = Queue()
+    dThreads = [DownloadThread(i, q) for i in xrange(1, 11)]
+
+    cEvent = Event()
+    tEvent = Event()
+
+    cThread = ConvertThread(q, cEvent, tEvent)
+
+    tThread = TarThread(cEvent, tEvent)
+    tThread.start()
+
+    for t in dThreads:
+        t.start()
+
+    cThread.start()
+
+    for t in dThreads:
+        t.join()
 
 q.put((-1, None)) #通知退出
-
